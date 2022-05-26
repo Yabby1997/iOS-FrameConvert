4833736bee5e284b6629cb6c1b7f0a29d2226b47
Seunghun <seunghun.yang@linecorp.com>
2022-05-26 10:48:02 +0900

    복잡한 뷰 계층에서 frame 얻어오기 구현

--- FrameConvert/ViewController.swift
+++ FrameConvert/ViewController.swift
@@ -8,47 +8,81 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
-    
-    private let someView: UIView = {
+class WrappingView: UIView {
+    private let targetView: UIView = {
         let view = UIView()
-        view.backgroundColor = .red
+        view.backgroundColor = .orange
         return view
     }()
-    
-    private let anotherView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .green
-        return view
-    }()
-    
-    private let anchorView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .cyan
-        return view
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.addSubview(someView)
-        someView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.width.height.equalTo(120)
-        }
-        
-        someView.addSubview(anotherView)
-        anotherView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.width.height.equalTo(50)
-        }
-        
-        view.addSubview(anchorView)
+
+    var targetViewFrame: CGRect { targetView.frame }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        anchorView.frame = anotherView.convert(anotherView.bounds, to: view)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        self.backgroundColor = .blue
+
+        addSubview(targetView)
+        targetView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(20)
+        }
     }
 }
 
+class ViewController: UIViewController {
+
+    private(set) lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.delegate = self
+        return view
+    }()
+
+    private let contentView = UIView()
+
+    private let wrappingView = WrappingView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .systemBackground
+
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.width.equalTo(view)
+            make.height.equalTo(1200)
+        }
+
+        contentView.addSubview(wrappingView)
+        wrappingView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(800)
+            make.leading.equalToSuperview().offset(100)
+            make.height.width.equalTo(100)
+        }
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let converted = wrappingView.convert(wrappingView.targetViewFrame, to: contentView)
+        let converted2 = contentView.convert(converted, to: scrollView)
+        let converted3 = scrollView.convert(converted2, to: view)
+
+        print(scrollView.contentOffset, converted3)
+    }
+}
